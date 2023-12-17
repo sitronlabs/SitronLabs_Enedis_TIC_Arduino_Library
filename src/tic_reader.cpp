e22e7fa2e1edecb500c7a1f58818a8f63315ee0c
Nicolas Schurando <nicolas@sitronlabs.com>
2023-12-17 01:16:08 +0100

    Improved reader class.

--- src/tic_reader.cpp
+++ src/tic_reader.cpp
@@ -20,10 +20,10 @@ int tic_reader::setup(Stream &uart) {
 
 /**
  * @brief
- * @param[out] message
+ * @param[out] dataset
  * @return
  */
-int tic_reader::process(struct tic_message &message) {
+int tic_reader::read(struct tic_dataset &dataset) {
 
     /* Ensure setup has been performed */
     if (m_stream == NULL) {
@@ -36,6 +36,7 @@ int tic_reader::process(struct tic_message &message) {
         /* Read byte */
         int rx = m_stream->read();
         if (rx < 0 || rx > UINT8_MAX) {
+            // Serial.println(" [e] Read error!");
             m_sm = STATE_0;
             return -EIO;
         }
@@ -43,13 +44,14 @@ int tic_reader::process(struct tic_message &message) {
         /* Perform parity check */
         if (__builtin_parity(rx) != 0) {
             m_sm = STATE_0;
+            // Serial.println(" [e] Parity error!");
             return -EIO;
         }
 
         /* Remove parity bit */
         rx = rx & 0x7F;
 
-        /* Parsing state machine */
+        /* Reception state machine */
         switch (m_sm) {
 
             case STATE_0: {  // Wait for frame start
@@ -61,10 +63,10 @@ int tic_reader::process(struct tic_message &message) {
                 break;
             }
 
-            case STATE_1: {  // Wait for either message start or frame stop
+            case STATE_1: {  // Wait for either dataset start or frame stop
 
-                if (rx == 0x0A) {  // Message start
-                    m_buffer_index = 0;
+                if (rx == 0x0A) {  // Dataset start
+                    m_dataset_buffer_index = 0;
                     m_sm = STATE_2;
                 } else if (rx == 0x03) {  // Frame stop
                     m_sm = STATE_0;
@@ -75,98 +77,129 @@ int tic_reader::process(struct tic_message &message) {
                 break;
             }
 
-            case STATE_2: {  // Parse message tag
+            case STATE_2: {  // Read dataset name
 
+                /* If we have received one of the two possible splitter chars,
+                 * take note of the type of splitter char as a way to differentiate between historic and standard datasets,
+                 * move on to processing the other parts of the dataset */
                 if (rx == 0x09 || rx == 0x20) {
-                    m_buffer[m_buffer_index] = rx;
-                    m_buffer_index++;
-                    m_splitter_char = rx;
+                    m_dataset_buffer[m_dataset_buffer_index] = rx;
+                    m_dataset_buffer_index++;
+                    m_splitter_char = (char)rx;
                     m_sm = STATE_3;
-                } else {
-                    if (m_buffer_index >= 8) {
+                }
+
+                /* Otherwise, keep appending */
+                else {
+                    if (m_dataset_buffer_index >= TIC_PARSER_DATASET_NAME_LENGTH_MAX) {
+                        // Serial.println(" [e] Dataset name too long!");
                         m_sm = STATE_0;
                         return -EIO;
                     } else {
-                        m_buffer[m_buffer_index] = rx;
-                        m_buffer_index++;
+                        m_dataset_buffer[m_dataset_buffer_index] = rx;
+                        m_dataset_buffer_index++;
                     }
                 }
                 break;
             }
 
-            case STATE_3: {  // Wait for message stop
+            case STATE_3: {  // Read dataset time and data
 
-                /* If we receive the end of message character */
+                /* If we have received the end of dataset character */
                 if (rx == 0x0D) {
 
                     /* Verify checksum:
-                     *  - for historic messages, where the splitter char is 0x20, the checksum doesn't include the last splitter char,
-                     *  - for standard messages, where the splitter char is 0x09, the checkum includes the last splitter char.
+                     *  - for historic datasets, where the splitter char is 0x20, the checksum doesn't include the last splitter char,
+                     *  - for standard datasets, where the splitter char is 0x09, the checkum includes the last splitter char.
                      * Go figure */
                     if (m_splitter_char == 0x20) {
-                        uint8_t checksum_received = m_buffer[m_buffer_index - 1];
+                        uint8_t checksum_received = m_dataset_buffer[m_dataset_buffer_index - 1];
                         uint8_t checksum_computed = 0x00;
-                        for (size_t i = 0; i < m_buffer_index - 2; i++) checksum_computed += m_buffer[i];
+                        for (uint8_t i = 0; i < m_dataset_buffer_index - 2; i++) {
+                            checksum_computed += m_dataset_buffer[i];
+                        }
                         checksum_computed = (checksum_computed & 0x3F) + 0x20;
                         if (checksum_computed != checksum_received) {
                             m_sm = STATE_0;
+                            Serial.println(" [e] Corrupt dataset!");
                             return -EIO;
                         }
                     } else if (m_splitter_char == 0x09) {
-                        uint8_t checksum_received = m_buffer[m_buffer_index - 1];
+                        uint8_t checksum_received = m_dataset_buffer[m_dataset_buffer_index - 1];
                         uint8_t checksum_computed = 0x00;
-                        for (size_t i = 0; i < m_buffer_index - 1; i++) checksum_computed += m_buffer[i];
+                        for (uint8_t i = 0; i < m_dataset_buffer_index - 1; i++) {
+                            checksum_computed += m_dataset_buffer[i];
+                        }
                         checksum_computed = (checksum_computed & 0x3F) + 0x20;
                         if (checksum_computed != checksum_received) {
                             m_sm = STATE_0;
+                            // Serial.println(" [e] Corrupt dataset!");
                             return -EIO;
                         }
                     } else {
                         m_sm = STATE_0;
+                        // Serial.println(" [e] Unsupported splitter char!");
                         return -EIO;
                     }
 
-                    /* Split message */
+                    /* Count number of splitters in buffer,
+                     * and while we are at it, replace splitters by null chars to make string processing easier */
                     uint8_t splitter_posistions[3] = {0};
                     uint8_t splitter_count = 0;
-                    for (uint8_t i = 0; i < m_buffer_index - 1; i++) {
-                        if (m_buffer[i] == m_splitter_char) {
+                    for (uint8_t i = 0; i < m_dataset_buffer_index - 1; i++) {
+                        if (m_dataset_buffer[i] == m_splitter_char) {
                             if (splitter_count < 3) {
                                 splitter_posistions[splitter_count] = i;
+                                m_dataset_buffer[i] = '\0';
                             }
                             splitter_count++;
                         }
                     }
 
-                    /* Fill message struct and move on */
+                    /* Fill dataset struct and move on */
                     if (splitter_count == 2) {
-                        strncpy(message.name, (char *)(&m_buffer[0]), splitter_posistions[0]);
-                        strncpy(message.data, (char *)(&m_buffer[splitter_posistions[0] + 1]), splitter_posistions[1] - (splitter_posistions[0] + 1));
-                        message.name[min(8, splitter_posistions[0])] = '\0';
-                        message.data[min(12, splitter_posistions[1] - (splitter_posistions[0] + 1))] = '\0';
+                        uint8_t name_pos = 0;
+                        uint8_t data_pos = splitter_posistions[0] + 1;
+                        uint8_t name_length = min(TIC_PARSER_DATASET_NAME_LENGTH_MAX, splitter_posistions[0]);
+                        uint8_t data_length = min(TIC_PARSER_DATASET_DATA_LENGTH_MAX, splitter_posistions[1] - (splitter_posistions[0] + 1));
+                        strncpy(dataset.name, (char *)(&m_dataset_buffer[name_pos]), name_length);
+                        strncpy(dataset.data, (char *)(&m_dataset_buffer[data_pos]), data_length);
+                        dataset.name[name_length] = '\0';
+                        dataset.time[0] = '\0';
+                        dataset.data[data_length] = '\0';
                         m_sm = STATE_1;
                         return 1;
                     } else if (splitter_count == 3) {
-                        strncpy(message.name, (char *)(&m_buffer[0]), splitter_posistions[0]);
-                        strncpy(message.data, (char *)(&m_buffer[splitter_posistions[1] + 1]), splitter_posistions[2] - (splitter_posistions[1] + 1));
-                        message.name[min(8, splitter_posistions[0])] = '\0';
-                        message.data[min(12, splitter_posistions[2] - (splitter_posistions[1] + 1))] = '\0';
+                        uint8_t name_pos = 0;
+                        uint8_t time_pos = splitter_posistions[0] + 1;
+                        uint8_t data_pos = splitter_posistions[1] + 1;
+                        uint8_t name_length = min(TIC_PARSER_DATASET_NAME_LENGTH_MAX, splitter_posistions[0]);
+                        uint8_t time_length = min(TIC_PARSER_DATASET_TIME_LENGTH_MAX, splitter_posistions[1] - (splitter_posistions[0] + 1));
+                        uint8_t data_length = min(TIC_PARSER_DATASET_DATA_LENGTH_MAX, splitter_posistions[2] - (splitter_posistions[1] + 1));
+                        strncpy(dataset.name, (char *)(&m_dataset_buffer[name_pos]), name_length);
+                        strncpy(dataset.time, (char *)(&m_dataset_buffer[time_pos]), time_length);
+                        strncpy(dataset.data, (char *)(&m_dataset_buffer[data_pos]), data_length);
+                        dataset.name[name_length] = '\0';
+                        dataset.time[time_length] = '\0';
+                        dataset.data[data_length] = '\0';
                         m_sm = STATE_1;
                         return 1;
                     } else {
+                        // Serial.println(" [e] Invalid splitter count!");
                         m_sm = STATE_0;
                         return -EIO;
                     }
                     break;
                 }
 
-                /* Otherwise, keep appending data to current message */
+                /* Otherwise, keep appending data to current dataset */
                 else {
-                    if (m_buffer_index >= 37) {
+                    if (m_dataset_buffer_index >= (TIC_PARSER_DATASET_NAME_LENGTH_MAX + 1 + TIC_PARSER_DATASET_TIME_LENGTH_MAX + 1 + TIC_PARSER_DATASET_DATA_LENGTH_MAX + 1 + TIC_PARSER_DATASET_CHECKSUM_LENGTH_MAX)) {
+                        // Serial.println(" [e] Dataset content too long!");
                         m_sm = STATE_0;
                     } else {
-                        m_buffer[m_buffer_index] = rx;
-                        m_buffer_index++;
+                        m_dataset_buffer[m_dataset_buffer_index] = rx;
+                        m_dataset_buffer_index++;
                     }
                 }
                 break;
@@ -179,6 +212,6 @@ int tic_reader::process(struct tic_message &message) {
         }
     }
 
-    /* Return no message */
+    /* Return no dataset */
     return 0;
 }
--- src/tic_reader.h
+++ src/tic_reader.h
@@ -8,24 +8,44 @@
 #include <errno.h>
 #include <stdint.h>
 
+/* Config */
+#define TIC_PARSER_DATASET_NAME_LENGTH_MAX 8
+#define TIC_PARSER_DATASET_TIME_LENGTH_MAX 7
+#define TIC_PARSER_DATASET_DATA_LENGTH_MAX 98
+#define TIC_PARSER_DATASET_CHECKSUM_LENGTH_MAX 1
+
 /**
- *
+ * Structure used to store a dataset when received.
  */
-struct tic_message {
-    char name[8 + 1];   //!< Label of the dataset (null-terminated)
-    char data[12 + 1];  //!< Content of the dataset (null-terminated)
+struct tic_dataset {
+    char name[TIC_PARSER_DATASET_NAME_LENGTH_MAX + 1];  //!< Label of the dataset (null-terminated).
+    char time[TIC_PARSER_DATASET_TIME_LENGTH_MAX + 1];  //!< Timestamp of the dataset (null-terminated).
+    char data[TIC_PARSER_DATASET_DATA_LENGTH_MAX + 1];  //!< Value of the dataset (null-terminated).
 };
 
 /**
- *
+ * Class to read datasets from Linky meters.
+ * Linky meters continously send frames of data, each frame composed of multiple datasets.
+ * You can think of a dataset as variable: it has a name and a value.
+ * An entire frame might be too big for the ram of some small microcontrollers, so this class allows to read datasets one by one.
  */
 class tic_reader {
    public:
-    /* Setup */
+    /**
+     * @brief Setup function.
+     * @param[in] uart A reference to the serial port class that will be used to read incoming bytes.
+     * @return 0 in case of success, or a negative error code otherwise.
+     * @note The serial will have to be initialized first with the correct baudrate (1200 for historic, 9600 for standard).
+     */
     int setup(Stream &uart);
 
-    /* Process incoming data */
-    int process(struct tic_message &message);
+    /**
+     * @brief Processes incoming data.
+     * @param[out] data A reference to a structure that will be filled when a complete dataset has been received.
+     * @return 1 when a complete dataset has been received, 0 when it still needs to be called, or a negative error code otherwise.
+     * @note Should be called regularly to treat bytes as they are received.
+     */
+    int read(struct tic_dataset &dataset);
 
    protected:
     Stream *m_stream = NULL;
@@ -36,9 +56,9 @@ class tic_reader {
         STATE_3,
         STATE_ERROR,
     } m_sm;
-    size_t m_buffer_index;
-    uint8_t m_buffer[8 + 1 + 13 + 1 + 12 + 1 + 1];
-    uint8_t m_splitter_char;
+    char m_splitter_char;
+    char m_dataset_buffer[TIC_PARSER_DATASET_NAME_LENGTH_MAX + 1 + TIC_PARSER_DATASET_TIME_LENGTH_MAX + 1 + TIC_PARSER_DATASET_DATA_LENGTH_MAX + 1 + TIC_PARSER_DATASET_CHECKSUM_LENGTH_MAX];
+    uint8_t m_dataset_buffer_index;
 };
 
 #endif
